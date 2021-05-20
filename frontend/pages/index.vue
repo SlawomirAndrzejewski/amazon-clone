a7d3f32f26c81cd73667a05f8e6bbbd54ce8d353
slawomir-andrzejewski <onewebdesignpl@gmail.com>
2021-05-20 22:26:30 +0200

    Add frontend

---
+++ frontend/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    <a href="#" class="a-button-buy-again">Buy Now!</a>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
